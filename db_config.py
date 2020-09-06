3069068a21fc9c87b9d70a61ca3935f67682e413
GOI17 <olivasgilberto@gmail.com>
2020-09-06 15:58:54 -0700

    :sparkles: DB
    created sqlite configuration

---
+++ db_config.py
@@ -0,0 +1,70 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection():
+    conn = None
+    print("Opening Database...")
+    try:
+        conn = sqlite3.connect("spellchecker.db")
+        print("Opened Database")
+        create_table(conn)
+    except Error as e:
+        raise e
+    finally:
+        if conn:
+            conn.close()
+
+
+def create_table(db):
+    db.execute(
+        """
+               CREATE TABLE REQUESTS (
+               ID INTEGER PRIMARY KEY AUTOINCREMENT,
+               HOST BLOB NOT NULL,
+               METHOD TEXT NOT NULL,
+               INPUT BLOB NOT NULL);
+            """
+    )
+    print("Created Requests table")
+
+
+def get_data():
+    conn = None
+    try:
+        conn = sqlite3.connect("spellchecker.db")
+        print("Opened Database")
+        sql = "SELECT HOST, METHOD, INPUT FROM REQUESTS"
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        conn.commit()
+
+        return rows
+    except Error:
+        raise Error
+    finally:
+        if conn:
+            conn.close()
+
+
+def insert_into_table(host, method, input):
+    conn = None
+    try:
+        conn = sqlite3.connect("spellchecker.db")
+        print("Opened Database")
+        sql = """ INSERT INTO REQUESTS(HOST, METHOD, INPUT)
+            VALUES(?,?,?) """
+        cursor = conn.cursor()
+        cursor.execute(sql, (host, method, input))
+        conn.commit()
+        print(f"Inserted ID: {cursor.lastrowid}")
+    except Error as e:
+        raise e
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == "__main__":
+    create_connection()
