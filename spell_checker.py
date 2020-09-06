2ad5e3d722a9dec82d8f5b3435bb421c4ac57240
GOI17 <olivasgilberto@gmail.com>
2020-09-06 13:28:41 -0700

    :bug: Fixed some bugs
    added comments and remove prints

--- spell_checker.py
+++ spell_checker.py
@@ -29,7 +29,10 @@ for word in WORDS:
     else:
         WORDS_INDEX[word] += 1
 
-
+# FIXED:
+# 1. one_length_edit was not called
+# 2. pass wrong arg to filter_real_words on single_word_possible_corrections
+# 3. Wrong order of the conditions
 def possible_corrections(word):
 
     single_word_possible_corrections = filter_real_words(word)
@@ -41,13 +44,18 @@ def possible_corrections(word):
     )
     no_correction_at_all = word
 
+    #print(f"Single word: {single_word_possible_corrections}")
+    #print(f"One Length word: {one_length_edit_possible_corrections}")
+    #print(f"Two Length word: {two_lenght_edit_possible_corrections}")
+    #print(f"No correction: {no_correction_at_all}")
+
     if single_word_possible_corrections:
         return single_word_possible_corrections
 
-    elif one_length_edit_possible_corrections:
+    if one_length_edit_possible_corrections:
         return one_length_edit_possible_corrections
 
-    elif two_lenght_edit_possible_corrections:
+    if two_lenght_edit_possible_corrections:
         return two_lenght_edit_possible_corrections
 
     else:
@@ -68,7 +76,6 @@ def spell_check_sentence(sentence):
 # FIXED: Random module was not imported
 def language_model(word):
 
-    print(max(sum(WORDS_INDEX.values()), random.randint(5, 137)))
     N = max(sum(WORDS_INDEX.values()), random.randint(5, 137))
     return WORDS_INDEX.get(word, 0) / N
 
@@ -132,44 +139,38 @@ def two_lenght_edit(word):
 
 def test_spell_check_sentence():
 
-    #sentence = "fabor guardar cilencio para no molestar"
-    #assert "favor guardar silencio para no molestar" == spell_check_sentence(
-    #    sentence
-    #)
+    sentence = "fabor guardar cilencio para no molestar"
+    assert "favor guardar silencio para no molestar" == spell_check_sentence(
+        sentence
+    )
 
-    #sentence = "un lgar para la hopinion"
+    sentence = "un lgar para la hopinion"
     # no ha lugar para la opinión
-    #assert "un lugar para la opinión" == spell_check_sentence(sentence)
+    assert "un lugar para la opinión" == spell_check_sentence(sentence)
 
     sentence = "el Arebol del día"
-    print(spell_check_sentence(sentence))
     assert "el arrebol del día" == spell_check_sentence(sentence)
 
-    #sentence = "Rezpeto por la educasión"
-    #print(spell_check_sentence(sentence))
-    #assert "respeto por la educación" == spell_check_sentence(sentence)
+    sentence = "Rezpeto por la educasión"
+    assert "respeto por la educación" == spell_check_sentence(sentence)
 
-    #sentence = "RTe encanta conduzir"
-    #print(spell_check_sentence(sentence))
-    #assert "te encanta conducir" == spell_check_sentence(sentence)
+    sentence = "RTe encanta conduzir"
+    assert "te encanta conducir" == spell_check_sentence(sentence)
 
-    #sentence = "HOy ay karne azada frezca siga pa dentro"
-    #print(spell_check_sentence(sentence))
-    #assert "hoy ay carne azada fresca siga la dentro" == spell_check_sentence(
-    #    sentence
-    #)
+    sentence = "HOy ay karne azada frezca siga pa dentro"
+    assert "hoy ay carne azada fresca siga la dentro" == spell_check_sentence(
+        sentence
+    )
 
-    #sentence = "En mi ezcuela no enseñan a escrivir ni a ler"
-    #result = spell_check_sentence(sentence)
-    #print(spell_check_sentence(sentence))
-    #assert "en mi escuela no enseñan a escribir ni a le" == sentence
+    sentence = "En mi ezcuela no enseñan a escrivir ni a ler"
+    assert (
+        "en mi escuela no enseñan a escribir ni a le" == spell_check_sentence(
+            sentence
+        )
+    )
 
-    #sentence = "él no era una persona de fiar pues era un mentirozo"
-    #print(spell_check_sentence(sentence))
-    #assert (
-    #    "él no era una persona de fiar pues era un mentiroso"
-    #    == spell_check_sentence(sentence)
-    #)
-
-
-test_spell_check_sentence()
+    sentence = "él no era una persona de fiar pues era un mentirozo"
+    assert (
+        "él no era una persona de fiar pues era un mentiroso"
+        == spell_check_sentence(sentence)
+    )
