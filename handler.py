422871424f72c56597fbd2199ef18ca386f220bb
GOI17 <olivasgilberto@gmail.com>
2020-09-06 15:59:58 -0700

    :sparkles: DB
    added insert on request and history

--- handler.py
+++ handler.py
@@ -1,18 +1,27 @@
 import json
 from spell_checker import spell_check_sentence
+from db_config import create_connection, insert_into_table, get_data
 
+# Uncomment only if the db not exists
+# create_connection()
 
 def spell_check(event, context):
-    input_sentence = json.loads(event.get("body")).get("text")
+    request_body = event.get("body")
+    request_host = "".join(
+        (event.get("headers").get("Host"), event.get("resource"))
+    )
+    request_method = event.get("httpMethod")
+    input_sentence = json.loads(request_body).get("text")
     correct_sentence = spell_check_sentence(input_sentence)
-
+    history = get_data()
     body = {
-        "text": correct_sentence
+        "text": correct_sentence,
+        "history": history
     }
-
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
+    insert_into_table(request_host, request_method, input_sentence)
 
     return response
