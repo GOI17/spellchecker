ee70995e987d775a5c247f5a7cc18eacfdce6c94
GOI17 <olivasgilberto@gmail.com>
2020-09-08 00:04:34 -0700

    :white_check_mark: Table Test
    added UI tests

--- spellchecker-frontend/src/tests/App.test.tsx
+++ spellchecker-frontend/src/tests/App.test.tsx
@@ -4,27 +4,24 @@ import App from '../components/App';
 
 beforeEach(() => render(<App />));
 
-test('Should display submit message button', () => {
-  screen.getByRole('button');
-});
+describe('UI Tests', () => {
+  test('Should display submit message button', () => {
+    screen.getByRole('button');
+  });
 
-test('Should display input message', () => {
-  screen.getByRole('textbox');
-});
+  test('Should display input message', () => {
+    screen.getByRole('textbox');
+  });
 
+  test('Should display loading message', () => {
+    screen.getByText('Fetching data...');
+  });
 
-test('Should display loading message', () => {
-  screen.getByText('Fetching data...');
+  test('Should display 4 table headers', async () => {
+    const [ number, host, method, message] = await screen.findAllByRole('columnheader');
+    expect(number).toBeInTheDocument();
+    expect(host).toBeInTheDocument();
+    expect(method).toBeInTheDocument();
+    expect(message).toBeInTheDocument();
+  });
 });
-/*
-test('Should display history table', () => {
-  screen.getByRole('table');
-});
-
-test('Should display 4 table headers', () => {
-  const [ number, host, method, message] = screen.getAllByRole('columnheader');
-  expect(number).toBeInTheDocument();
-  expect(host).toBeInTheDocument();
-  expect(method).toBeInTheDocument();
-  expect(message).toBeInTheDocument();
-});*/
